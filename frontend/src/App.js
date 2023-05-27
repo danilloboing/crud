c86127a16c20d65d31834e855348b992ea7319b3
Danillo <danillo.boing.souza@gmail.com>
2023-05-26 21:13:57 -0300

    change paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CarRegistration from "./pages/CadastroCarros/CarRegistration";
 import LoginPage from "./pages/Login/LoginPage";
+import CadastroUsuario from "./pages/CadastroUser/CadastroUsuario";
 
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={LoginPage} />
-        <Route exact path="/home" component={CarRegistration} />
+
+      <Routes>
         
-      </Switch>
+        <Route exact path="/" element={<LoginPage/>} />
+        <Route path="/cadastro" element={<CadastroUsuario/>} />
+        <Route path="/home" element={<CarRegistration/>} />
+        
+      </Routes>
     </Router>
   );
 }
